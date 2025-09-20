9a321eae58e379f7c018f33854c57375f1bd4d9c
harshit-yc <harshit@yosemitecrew.com>
2025-09-21 00:07:11 +0530

    Sample Images Tested for MultiPlatform Compatibility

--- apps/mobileAppYC/App.tsx
+++ apps/mobileAppYC/App.tsx
@@ -4,9 +4,16 @@
  *
  * @format
  */
-
+import { SvgUri } from 'react-native-svg';
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  Image,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
@@ -28,6 +35,33 @@ function AppContent() {
 
   return (
     <View style={styles.container}>
+      <View>
+        <Text>Harshit Wandhare</Text>
+      </View>
+            <View>
+        
+    <SvgUri
+  width="100"
+  height="100"
+  uri="https://reactnative.dev/img/header_logo.svg"
+/>
+<Image
+  source={{ uri: 'https://picsum.photos/200/200' }}
+  style={{ width: 100, height: 100 }}
+  onLoad={() => console.log('Image loaded')}
+  onError={(error) => console.log('Image error:', error)}
+/>
+<Image
+  source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+  style={{ width: 100, height: 100 }}
+/>
+ <Image
+          source={require('./assets/sample.jpg')} // Replace with your actual image name
+          style={{ width: 100, height: 100 }}
+          onLoad={() => console.log('Local image loaded')}
+          onError={(error) => console.log('Local image error:', error)}
+        />
+      </View>
       <NewAppScreen
         templateFileName="App.tsx"
         safeAreaInsets={safeAreaInsets}
