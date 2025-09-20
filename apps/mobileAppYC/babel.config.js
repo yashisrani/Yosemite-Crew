0f8a1f3e670317acdb24dca4754f4e1bf4ecef5d
harshit-yc <harshit@yosemitecrew.com>
2025-09-21 01:41:26 +0530

    Project Structure Finalized

--- apps/mobileAppYC/babel.config.js
+++ apps/mobileAppYC/babel.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-     plugins: [
+     plugins: [ [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
       'react-native-worklets/plugin',
     ],
 };
